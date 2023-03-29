a481962d4b92ba2fc47926b9b579130d61611390
Andrey <andrey.igorevich.92@gmail.com>
2023-03-29 16:45:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// // "use sctrict"
+
+// // // let number = 4.6;
+
+// // // console.log(-4/0);
+// // // console.log('string'* 9);
+
+// // const persone = `5`;
+
+// // const bool = false; 
+
+// // console.log(something);
+
+// // let und;
+// // console.log
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name) 
+// console.log(obj["name"]);
+
+// let arr = ["plum.png","orange.jpg", 6, "apple.bmp", {}, []];
+// console.log(arr[1]);
+
+// const arr = ["a", "b", "c"];
+// const arrObj = {
+//     a: "a",
+//     "1": "b",
+//     2: "c"
+// }
+
+// // arrObj.b = "1234";
+// arrObj["b"] = "1234";
+
+// console.log(arrObj["b"]);
+// console.log(arrObj.b);
+
+// const obj = {a: 1, b: 2};
+
+// const obj = {
+//     "Anna": 500,
+//     "Alice": 800,
+// };
+
+
+
+// alert("hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть 18?", "18" );
+// console.log(typeof(answer));
+
+// const answer = [];
+
+// answer[0] = prompt("Как ваше имя?", "");
+// answer[1] = prompt("Как ваша фамилия?", "");
+// answer[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof(ans));
+// console.log(typeof(null));
+
+
+"use sctrict";
+
+const category = "toys";
+
+// console.log("https://someurl.com/" + category + "/" + "4");
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
